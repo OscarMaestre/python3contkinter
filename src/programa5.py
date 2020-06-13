15af78c1089067721295193ce78a8cbe512fc166
Oscar GG <profesor.oscar.gomez@gmail.com>
2020-06-13 19:06:44 +0200

    Reconstruccion

---
+++ src/programa5.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import tkinter as tk
+
+raiz=tk.Tk()
+
+MAX_FILAS=3
+MAX_COLUMNAS=4
+
+for fila in range(0, MAX_FILAS):
+    for columna in range(0, MAX_COLUMNAS):
+        texto="Boton en ({0},{1})".format(fila, columna)
+        boton=tk.Button(raiz, text=texto)
+        boton.grid(row=fila, column=columna)
+
+
+boton_extra=tk.Button(raiz, text="Boton en (3,1)")
+#Nuestro control se expande para poder "pegarse" a los lados
+boton_extra.grid(row=3, column=1, columnspan=2, sticky="ew")
+
+
+for fila in range(0, MAX_FILAS+1):
+    raiz.grid_rowconfigure(fila, weight=1)
+    
+raiz.mainloop()
